1d8a6a7e833a3217d4ce9ec6babd03c4a3f4d5b1
David Barrera <davidbarrera89@outlook.com>
2022-12-08 17:59:12 -0500

    Agregar archivos de proyecto.

---
+++ BjornApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using BjornApp.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BjornApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Presentacion();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
